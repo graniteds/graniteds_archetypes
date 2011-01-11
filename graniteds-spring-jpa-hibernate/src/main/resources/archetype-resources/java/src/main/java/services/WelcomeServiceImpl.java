7403101a8d79364794539de662146c854465d8a4
fwolff <franck.wolff@graniteds.org>
2011-01-11 15:03:24 +0100

    Initial Git Commit
    Signed-off-by: fwolff <franck.wolff@graniteds.org>

---
+++ graniteds-spring-jpa-hibernate/src/main/resources/archetype-resources/java/src/main/java/services/WelcomeServiceImpl.java
@@ -0,0 +1,66 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+  GRANITE DATA SERVICES
+  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
+
+  This file is part of Granite Data Services.
+
+  Granite Data Services is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or (at your
+  option) any later version.
+
+  Granite Data Services is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+  for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ${package}.services;
+
+import javax.persistence.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.NoResultException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import org.granite.messaging.service.annotations.RemoteDestination;
+
+import ${package}.entities.Welcome;
+
+
+@Service("welcomeService")
+@RemoteDestination(id="welcomeService", source="welcomeService")
+public class WelcomeServiceImpl implements WelcomeService {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+
+    @Transactional
+    public Welcome hello(String name) {
+        if (name == null || name.trim().length() == 0)
+            throw new RuntimeException("Name cannot be null or empty");
+        
+        Welcome welcome = null;
+        try {
+            Query q = entityManager.createQuery("select w from Welcome w where w.name = :name");
+            q.setParameter("name", name);
+            welcome = (Welcome)q.getSingleResult();
+        }
+        catch (NoResultException e) {
+            welcome = new Welcome();
+            welcome.setName(name);
+            welcome.setMessage("Welcome " + name);
+            entityManager.persist(welcome);
+        }
+        return welcome;
+    }
+}
---
+++ graniteds-tide-cdi-jpa/src/main/resources/archetype-resources/java/src/main/java/services/WelcomeService.java
@@ -0,0 +1,43 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+  GRANITE DATA SERVICES
+  Copyright (C) 2011 GRANITE DATA SERVICES S.A.S.
+
+  This file is part of Granite Data Services.
+
+  Granite Data Services is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or (at your
+  option) any later version.
+
+  Granite Data Services is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+  for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package ${package}.services;
+
+import java.util.List;
+
+import org.granite.messaging.service.annotations.RemoteDestination;
+
+import org.granite.tide.data.DataEnabled;
+import org.granite.tide.data.DataEnabled.PublishMode;
+
+import ${package}.entities.Welcome;
+
+
+@RemoteDestination
+@DataEnabled(topic="welcomeTopic", params=ObserveAllPublishAll.class, publish=PublishMode.ON_SUCCESS)
+public interface WelcomeService {
+
+    public Welcome hello(String name);
+    
+    public List<Welcome> findAll();
+}
